e1dc9184b7bae6d941b98e0b6784ff5db37ab024
AndriiKhalin <danoking2002@gmail.com>
2025-10-15 22:01:39 +0200

    Add project files.

---
+++ backend/src/EventManagement.BusinessLogicLayer/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EventManagement.BusinessLogicLayer
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ backend/src/EventManagement.DataAccessLayer/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EventManagement.DataAccessLayer
+{
+    public class Class1
+    {
+
+    }
+}
